745f1821edd27477f97aa253d28da1a7af64c3ca
Muhammet Enes <muhammetxx310@gmail.com>
2025-04-04 22:22:56 +0300

    Add files via upload

---
+++ Discord Bot dashboard 2/script.js
@@ -0,0 +1,8 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const mobileMenu = document.getElementById('mobile-menu');
+    const navLinks = document.querySelector('.nav-links');
+
+    mobileMenu.addEventListener('click', () => {
+        navLinks.classList.toggle('active');
+    });
+});
